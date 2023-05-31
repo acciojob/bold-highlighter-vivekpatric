a292e6507ccaad5c9f94fae34abf3086ee2dc00f
Vivek Patric R <96299357+vivekpatric@users.noreply.github.com>
2023-05-31 22:30:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+
 function highlight() {
     //Write your code here
-
+ let bold=document.getElementsByTagName("strong");
+	for(let i=0;i<bold.length;i++){
+		bold[i].style.color="green";
+	}
+	
 
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+let bold=document.getElementsByTagName("strong");
+	for(let i=0;i<bold.length;i++){
+		bold[i].style.color="black";
+	}
 }
